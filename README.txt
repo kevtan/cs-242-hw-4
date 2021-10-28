93cfaa39075ba57fd7afe4d87ade2cbfb2819073
Kevin Tan <tankevin@stanford.edu>
2021-10-27 19:53:13 -0700

    :tada: Upload homework 4 starter code

---
+++ README.txt
@@ -0,0 +1,2 @@
+# Put your SUID number here (eg. id: 01234567)
+id: <your_8-digit_student_id_number>
---
+++ translate.py
@@ -0,0 +1,6 @@
+
+import src.lam as lam
+import src.pi as pi
+
+def translate(e: lam.Expr, channel: str) -> pi.Proc:
+    pass
