ae9214f0ff3fae53d7f9110942fd65960395be2e
Kevin Tan <tankevin@stanford.edu>
2021-10-30 16:53:51 -0700

    🐛 Return variable strings instead of VarProc

--- translate.py
+++ translate.py
@@ -7,12 +7,12 @@ _fresh_u_variable_counter = 0
 _fresh_v_variable_counter = 0
 
 
-def generate_fresh_variable(letter: str) -> pi.VarProc:
+def generate_fresh_variable(letter: str) -> str:
     assert letter in {"c", "d", "u", "v"}
     global_variable_name = f"_fresh_{letter}_variable_counter"
     fresh_variable_name = f"_{letter}{globals()[global_variable_name]}"
     globals()[global_variable_name] += 1
-    return pi.VarProc(fresh_variable_name)
+    return fresh_variable_name
 
 
 def translate(e: lam.Expr, channel: str) -> pi.Proc:
