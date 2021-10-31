8dbf40e84d9b57ff5a13333768e7f25de11896c4
Kevin Tan <tankevin@stanford.edu>
2021-10-30 17:11:13 -0700

    ✨ Implement translation from lambda to pi calculus

--- translate.py
+++ translate.py
@@ -16,4 +16,29 @@ def generate_fresh_variable(letter: str) -> str:
 
 
 def translate(e: lam.Expr, channel: str) -> pi.Proc:
-    pass
+    if isinstance(e, lam.Var):
+        # Translate a variable into π-calculus.
+        return pi.Send(channel, e.s, pi.Parallel([]))
+    elif isinstance(e, lam.Lam):
+        # Translate an abstraction into π-calculus.
+        u = generate_fresh_variable("u")
+        return pi.Receive(e.s, channel, pi.Receive(u, channel, translate(e.e, u)))
+    else:
+        # Translate an application into π-calculus.
+        assert isinstance(e, lam.App)
+        c = generate_fresh_variable("c")
+        d = generate_fresh_variable("d")
+        v = generate_fresh_variable("v")
+        return pi.Nu(
+            c,
+            pi.Nu(
+                d,
+                pi.Parallel(
+                    [
+                        translate(e.e1, c),
+                        pi.Send(d, c, pi.Send(channel, c, pi.Parallel([]))),
+                        pi.Replicate(pi.Receive(v, d, translate(e.e2, v))),
+                    ]
+                ),
+            ),
+        )
